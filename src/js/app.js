bd38b77ccac12664232ab68c73c530d368d775a5
Sanshai <vbn-134@yandex.ru>
2023-08-22 15:06:58 +0300

    исправление ошибок lint

--- src/js/app.js
+++ src/js/app.js
@@ -1,56 +1,53 @@
-const goblin = Array.from(document.querySelectorAll('.game-box'));
+const goblin = Array.from(document.querySelectorAll(".game-box"));
 const len = goblin.length;
 const numr = () => Math.floor(Math.random() * len);
-const wins = document.querySelector('.status__wins');
-const loss = document.querySelector('.status__loss');
-const stat = document.querySelector('.stat');
-const box = document.querySelector('.box');
-const info = document.querySelector('.info');
-
+const wins = document.querySelector(".status__wins");
+const loss = document.querySelector(".status__loss");
+const stat = document.querySelector(".stat");
+const info = document.querySelector(".info");
 
 function game() {
-    goblin.forEach((item) => {
-        item.addEventListener('click', () => {
-            if (stat.textContent == 1) {
-                info.classList.add('win')
-                info.textContent = 'Вы выиграли!!!'
-                reset();
-            }
-            if (item.className === 'game-box goblin') {
-                ++wins.textContent;
-                --stat.textContent;
-            } else {
-                if (loss.textContent == 4) {
-                    info.classList.add('win')
-                    info.textContent = 'Вы проиграли((('
-                    reset();
-                }
-                ++loss.textContent;
-            }
-        });
+  goblin.forEach((item) => {
+    item.addEventListener("click", () => {
+      if (stat.textContent == 1) {
+        info.classList.add("win");
+        info.textContent = "Вы выиграли!!!";
+        reset();
+      }
+      if (item.className === "game-box goblin") {
+        ++wins.textContent;
+        --stat.textContent;
+      } else {
+        if (loss.textContent == 4) {
+          info.classList.add("win");
+          info.textContent = "Вы проиграли(((";
+          reset();
+        }
+        ++loss.textContent;
+      }
     });
+  });
 }
 
 function reset() {
-    setTimeout(() => {
-        info.textContent = 'Долбани гоблина!'
-        wins.textContent = 0;
-        loss.textContent = 0;
-        stat.textContent = 5;
-        info.classList.remove('win')
-    }, 2000)
+  setTimeout(() => {
+    info.textContent = "Долбани гоблина!";
+    wins.textContent = 0;
+    loss.textContent = 0;
+    stat.textContent = 5;
+    info.classList.remove("win");
+  }, 2000);
 }
 
 setInterval(jump, 1000);
 
 function jump() {
-    goblin.forEach((item) => {
-        if (item.className === 'game-box goblin') {
-            item.classList.remove('goblin');
-        }
-    });
-    goblin[numr()].classList.add('goblin');
+  goblin.forEach((item) => {
+    if (item.className === "game-box goblin") {
+      item.classList.remove("goblin");
+    }
+  });
+  goblin[numr()].classList.add("goblin");
 }
 
 game();
-
