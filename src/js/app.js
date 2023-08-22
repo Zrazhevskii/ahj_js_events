6a5e75d7986e65d5c9ef93ebd3538e44d5ff562e
Sanshai <vbn-134@yandex.ru>
2023-08-22 15:03:52 +0300

    написание функеции, сборка

--- src/js/app.js
+++ src/js/app.js
@@ -1,14 +1,56 @@
-const goblin = Array.from(document.querySelectorAll(".game-box"));
+const goblin = Array.from(document.querySelectorAll('.game-box'));
 const len = goblin.length;
 const numr = () => Math.floor(Math.random() * len);
+const wins = document.querySelector('.status__wins');
+const loss = document.querySelector('.status__loss');
+const stat = document.querySelector('.stat');
+const box = document.querySelector('.box');
+const info = document.querySelector('.info');
 
-setInterval(game, 1000);
 
 function game() {
-  goblin.forEach((item) => {
-    if (item.className == "game-box goblin") {
-      item.classList.remove("goblin");
-    }
-  });
-  goblin[numr()].classList.add("goblin");
+    goblin.forEach((item) => {
+        item.addEventListener('click', () => {
+            if (stat.textContent == 1) {
+                info.classList.add('win')
+                info.textContent = 'Вы выиграли!!!'
+                reset();
+            }
+            if (item.className === 'game-box goblin') {
+                ++wins.textContent;
+                --stat.textContent;
+            } else {
+                if (loss.textContent == 4) {
+                    info.classList.add('win')
+                    info.textContent = 'Вы проиграли((('
+                    reset();
+                }
+                ++loss.textContent;
+            }
+        });
+    });
 }
+
+function reset() {
+    setTimeout(() => {
+        info.textContent = 'Долбани гоблина!'
+        wins.textContent = 0;
+        loss.textContent = 0;
+        stat.textContent = 5;
+        info.classList.remove('win')
+    }, 2000)
+}
+
+setInterval(jump, 1000);
+
+function jump() {
+    goblin.forEach((item) => {
+        if (item.className === 'game-box goblin') {
+            item.classList.remove('goblin');
+        }
+    });
+    goblin[numr()].classList.add('goblin');
+}
+
+game();
+
